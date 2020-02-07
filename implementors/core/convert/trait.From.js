689d4e85974b9c42a6f82145f55727c455aadf19
syntacticsugarglider <zenerboson@gmail.com>
2020-02-07 20:26:18 +0000

    Deploying to gh-pages from master 857e0e4c0cb490fd43ae169969fa1e8461710708

---
+++ implementors/core/convert/trait.From.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["bitbuf_vlq"] = [{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u64.html\">u64</a>&gt;&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;T&gt; for <a class=\"struct\" href=\"bitbuf_vlq/struct.Vlq.html\" title=\"struct bitbuf_vlq::Vlq\">Vlq</a>","synthetic":false,"types":["bitbuf_vlq::Vlq"]},{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;CopyError&gt; for <a class=\"enum\" href=\"bitbuf_vlq/enum.Error.html\" title=\"enum bitbuf_vlq::Error\">Error</a>","synthetic":false,"types":["bitbuf_vlq::Error"]}];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
---
+++ implementors/core/marker/trait.Send.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["bitbuf_vlq"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"bitbuf_vlq/struct.Vlq.html\" title=\"struct bitbuf_vlq::Vlq\">Vlq</a>","synthetic":true,"types":["bitbuf_vlq::Vlq"]},{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"enum\" href=\"bitbuf_vlq/enum.Error.html\" title=\"enum bitbuf_vlq::Error\">Error</a>","synthetic":true,"types":["bitbuf_vlq::Error"]}];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
